ecceb88c3dd864575b43a7360003b91d4b426f44
Federico Capoano <f.capoano@openwisp.io>
2024-09-20 18:07:01 -0400

    [chores] Prepared for publishing

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,7 +27,7 @@ MENU = {
         'Community Support': 'support/',
         'Commercial Support': 'commercial-support/',
     },
-    'Docs': 'https://openwisp.io/docs/preview/dev/',
+    'Docs': 'https://openwisp.io/docs/dev/',
     'Blog': 'blog/',
 }
 
