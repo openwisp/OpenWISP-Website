b91c6332eeb84a40960e042781cb67a0e809d1a9
Federico Capoano <f.capoano@openwisp.io>
2025-09-08 21:39:39 -0300

    [chores] Embed Inter woff2 inline

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,15 @@ const purgecss = require("@fullhuman/postcss-purgecss");
 const cssnano = require("cssnano");
 const pruneVar = require("postcss-prune-var");
 const varCompress = require("postcss-variable-compress");
-const importUrl = require("postcss-import-url");
-const postcssImport = require("postcss-import");
+const postcssUrl = require("postcss-url");
 
 module.exports = {
   plugins: [
-    // embed imports
-    importUrl({
-      recursive: true,
-      resolveUrls: true,
-      modernBrowser: true,
+    postcssUrl({
+      url: "inline",
+      filter: /Inter-normal-\d+\.woff2$/,
+      maxSize: Infinity,
     }),
-    postcssImport(),
     // remove unused CSS
     purgecss({
       content: ["**/*.html", "js/activity/feed.js"],
